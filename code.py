fff39ccaac6180be823dbdd8998c98f6fedb0c85
kimleng766 <kimleng@api-school.com>
2025-07-16 08:03:12 +0700

    Create code.py

---
+++ code.py
@@ -0,0 +1,13 @@
+answer = input("Are you running today? Yes or No? ")
+if answer == "Yes":
+    print("Yay!")
+else:
+    answer = input("Okay, how about cycling? Yes or No? ")
+    if answer == "Yes":
+        print("Yay!")
+    else:
+        answer = input("Then you're left with swimminh? Yes or No? ")
+        if answer == "Yes":
+             print("Yay!")
+        else:
+            print("Other people are working hard while you're lazy. They want it more than you")
